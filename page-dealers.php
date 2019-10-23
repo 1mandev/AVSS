bb6923d825a18854961110af945177dfe2646817
Mr-spShuvo <mr.shahriyarshuvo@gmail.com>
2019-10-23 15:42:30 +0600

    final push v-1.0.1

--- page-dealers.php
+++ page-dealers.php
@@ -12,28 +12,27 @@
 
     <!-- dealer -->
     <section class="dealer">
-        <div class="row mx-auto">
-            <div class="col-md-8 mx-auto">
-                <div class="avss-section-intro text-center">
-                    <h1 class="avss-section-intro__header">
-                        FIND YOUR NEAREST dealer
-                    </h1>
-                    <div class="line-small mx-auto my-4"></div>
-                </div>
+        <div class="mx-auto">
+            <div class="avss-section-intro text-center">
+                <h1 class="avss-section-intro__header">
+                    FIND YOUR NEAREST dealer
+                </h1>
+                <div class="line-small mx-auto my-4"></div>
             </div>
         </div>
+
 
         <div class="container">
             <div class="row justify-content-center">
                 <?php
-                    $dealers = new WP_Query(
-                        array(
-                            'post_type' => 'dealers',
-                            'posts_per_page' => -1,
-                        )
-                    );
-                    while ($dealers->have_posts()) : $dealers->the_post();
-                ?>
+                $dealers = new WP_Query(
+                    array(
+                        'post_type' => 'dealers',
+                        'posts_per_page' => -1,
+                    )
+                );
+                while ($dealers->have_posts()) : $dealers->the_post();
+                    ?>
                     <div class="col-md-4 pb-5">
                         <div class="card h-100 card-custom">
                             <div class="card-body text-center">
@@ -41,24 +40,29 @@
                                     <div class="product__view--icon">
                                         <i class="icon ion-md-pin"></i>
                                     </div>
-                                    <h3 class="avss-devices__device--title title-big my-4 pt-1">
-                                        <?php the_field('dealer_title'); ?>
+                                    <h3 class="avss-devices__device--zilla mt-4 pt-1">
+                                        <?php the_field('dealer_zilla'); ?>
                                     </h3>
                                 </div>
-                                <p class="avss-devices__description text-center pb-3 px-sm-3 px-md-0">
+                                <p class="avss-devices__description text-center pb-3 pb-sm-3 pb-md-0">
+                                    <p class="avss-devices__device--title">
+                                        <?php the_field('dealer_title'); ?>
+                                    </p>
                                     <p>
                                         <?php the_field('dealer_address'); ?>
                                     </p>
-                                    <a href="phone:<?php echo get_field('dealer_phone_num_1') ?>" class="font-weight-bold"><?php the_field('dealer_phone_num_1'); ?></a>
+                                    <a href="tel:<?php echo get_field('dealer_phone_num_1') ?>" class="font-weight-bold"><?php the_field('dealer_phone_num_1'); ?></a>
                                     <br>
-                                    <a href="phone:<?php echo get_field('dealer_phone_num_2') ?>" class="font-weight-bold d-block pt-2"><?php the_field('dealer_phone_num_2'); ?></a>
+                                    <a href="tel:<?php echo get_field('dealer_phone_num_2') ?>" class="font-weight-bold d-block pt-2"><?php the_field('dealer_phone_num_2'); ?></a>
+                                </p>
                             </div>
                         </div>
                     </div>
                 <?php endwhile;
-                wp_reset_query(); ?>
+                    wp_reset_query(); ?>
             </div>
         </div>
+
 
         <div class="contact-button text-center mt-5">
             <a href="<?php echo site_url('/contact'); ?>" class="btn-custom btn-primary-custom btn-lg-custom box-shadow-btn">
